43ea6622b0697c426cf290eed7ace2eacd306633
Scott Aslan <scottyaslan@gmail.com>
2018-04-30 15:07:17 -0400

    [NIFIREG-168] include webapp in src to facilitate local build and review process

--- gh-pages.systemjs.config.js
+++ gh-pages.systemjs.config.js
@@ -19,12 +19,12 @@
     System.config({
         paths: {
             // paths serve as alias
-            'npm:': 'fds/node_modules/'
+            'npm:': 'node_modules/'
         },
         // map tells the System loader where to look for things
         map: {
             'text': 'npm:systemjs-plugin-text/text.js',
-            'app': './platform',
+            'app': './webapp',
 
             // jquery
             'jquery': 'npm:jquery/dist/jquery.min.js',
@@ -40,27 +40,7 @@
             '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
             '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
             '@angular/flex-layout': 'npm:@angular/flex-layout/bundles/flex-layout.umd.js',
-            '@angular/flex-layout/core': 'npm:@angular/flex-layout/bundles/flex-layout-core.umd.js',
-            '@angular/flex-layout/extended': 'npm:@angular/flex-layout/bundles/flex-layout-extended.umd.js',
-            '@angular/flex-layout/flex': 'npm:@angular/flex-layout/bundles/flex-layout-flex.umd.js',
             '@angular/material': 'npm:@angular/material/bundles/material.umd.js',
-            '@angular/material/core': 'npm:@angular/material/bundles/material-core.umd.js',
-            '@angular/material/card': 'npm:@angular/material/bundles/material-card.umd.js',
-            '@angular/material/divider': 'npm:@angular/material/bundles/material-divider.umd.js',
-            '@angular/material/progress-bar': 'npm:@angular/material/bundles/material-progress-bar.umd.js',
-            '@angular/material/progress-spinner': 'npm:@angular/material/bundles/material-progress-spinner.umd.js',
-            '@angular/material/chips': 'npm:@angular/material/bundles/material-chips.umd.js',
-            '@angular/material/input': 'npm:@angular/material/bundles/material-input.umd.js',
-            '@angular/material/icon': 'npm:@angular/material/bundles/material-icon.umd.js',
-            '@angular/material/button': 'npm:@angular/material/bundles/material-button.umd.js',
-            '@angular/material/checkbox': 'npm:@angular/material/bundles/material-checkbox.umd.js',
-            '@angular/material/tooltip': 'npm:@angular/material/bundles/material-tooltip.umd.js',
-            '@angular/material/dialog': 'npm:@angular/material/bundles/material-dialog.umd.js',
-            '@angular/material/sidenav': 'npm:@angular/material/bundles/material-sidenav.umd.js',
-            '@angular/material/menu': 'npm:@angular/material/bundles/material-menu.umd.js',
-            '@angular/material/form-field': 'npm:@angular/material/bundles/material-form-field.umd.js',
-            '@angular/material/toolbar': 'npm:@angular/material/bundles/material-toolbar.umd.js',
-            '@angular/material/autocomplete': 'npm:@angular/material/bundles/material-autocomplete.umd.js',
             '@angular/platform-browser/animations': 'npm:@angular/platform-browser/bundles/platform-browser-animations.umd.js',
             '@angular/cdk': 'npm:@angular/cdk/bundles/cdk.umd.js',
             '@angular/cdk/a11y': 'npm:@angular/cdk/bundles/cdk-a11y.umd.js',
@@ -86,28 +66,53 @@
             'hammerjs': 'npm:hammerjs/hammer.min.js',
 
             // Covalent
-            '@covalent/core': 'npm:@covalent/core/bundles/covalent-core.umd.min.js',
-            '@covalent/core/common': 'npm:@covalent/core/bundles/covalent-core-common.umd.min.js',
+            '@covalent/core': 'npm:@covalent/core/bundles/core.umd.min.js',
 
             // other libraries
             'rxjs': 'npm:rxjs',
             'zone.js': 'npm:zone.js/dist/zone.js',
+            'core-js': 'npm:core-js/client/shim.min.js',
+            'superagent': 'npm:superagent/superagent.js',
+            'querystring': 'npm:querystring',
+            'tslib': 'npm:tslib/tslib.js',
 
             // Fluid Design System
-            '@fluid-design-system/core': 'platform/core/fluid-design-system.module.js',
-            '@fluid-design-system/dialogs': 'platform/core/dialogs/fds-dialogs.module.js',
-            '@fluid-design-system/dialog-component': 'platform/core/dialogs/fds-dialog.component.js',
-            '@fluid-design-system/dialog-service': 'platform/core/dialogs/services/dialog.service.js',
-            '@fluid-design-system/confirm-dialog-component': 'platform/core/dialogs/confirm-dialog/confirm-dialog.component.js',
-            '@fluid-design-system/snackbars': 'platform/core/snackbars/fds-snackbars.module.js',
-            '@fluid-design-system/snackbar-component': 'platform/core/snackbars/fds-snackbar.component.js',
-            '@fluid-design-system/snackbar-service': 'platform/core/snackbars/services/snackbar.service.js',
-            '@fluid-design-system/coaster-component': 'platform/core/snackbars/coaster/coaster.component.js',
-            '@fluid-design-system/common/storage-service': 'platform/core/common/services/fds-storage.service.js'
+            '@fluid-design-system/core': 'npm:nifi-fds/platform/core/fluid-design-system.module.js',
+            '@fluid-design-system/dialogs': 'npm:nifi-fds/platform/core/dialogs/fds-dialogs.module.js',
+            '@fluid-design-system/dialog-component': 'npm:nifi-fds/platform/core/dialogs/fds-dialog.component.js',
+            '@fluid-design-system/dialog-service': 'npm:nifi-fds/platform/core/dialogs/services/dialog.service.js',
+            '@fluid-design-system/confirm-dialog-component': 'npm:nifi-fds/platform/core/dialogs/confirm-dialog/confirm-dialog.component.js',
+            '@fluid-design-system/snackbars': 'npm:nifi-fds/platform/core/snackbars/fds-snackbars.module.js',
+            '@fluid-design-system/snackbar-component': 'npm:nifi-fds/platform/core/snackbars/fds-snackbar.component.js',
+            '@fluid-design-system/snackbar-service': 'npm:nifi-fds/platform/core/snackbars/services/snackbar.service.js',
+            '@fluid-design-system/coaster-component': 'npm:nifi-fds/platform/core/snackbars/coaster/coaster.component.js'
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
+            app: {
+                defaultExtension: 'js',
+                meta: {
+                    './*.js': {
+                        loader: 'webapp/systemjs-angular-loader.js'
+                    }
+                }
+            },
+            'webapp/systemjs-angular-loader.js': {
+                loader: false
+            },
             'rxjs': {
+                defaultExtension: 'js'
+            },
+            'querystring': {
+                main: './index.js',
+                defaultExtension: 'js'
+            },
+            'moment': {
+                main: './moment.js',
+                defaultExtension: 'js'
+            },
+            'angular2-moment': {
+                main: './index.js',
                 defaultExtension: 'js'
             }
         }
